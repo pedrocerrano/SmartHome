5c196d11974a38268714db1545dce9eee086be50
Trevor Adcock <trevor.adcock@mac.com>
2021-12-21 15:32:14 -0800

    Initial Commit creating xcode project

---
+++ Messages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messages
+//
+//  Created by Trevor Adcock on 12/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
